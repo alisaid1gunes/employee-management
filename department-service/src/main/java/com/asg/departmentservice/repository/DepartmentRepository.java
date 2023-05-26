d96b4ca26487c9393869191adeffbfd8123caa0a
alisaidgunes <alisaidgunes1@gmail.com>
2023-05-26 03:48:32 +0300

    Add: Department Repository for department service

---
+++ department-service/src/main/java/com/asg/departmentservice/repository/DepartmentRepository.java
@@ -0,0 +1,7 @@
+package com.asg.departmentservice.repository;
+
+import com.asg.departmentservice.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+}
