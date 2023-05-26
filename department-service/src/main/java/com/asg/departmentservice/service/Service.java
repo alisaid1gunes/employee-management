112748a6e57d0dab43fcfa2fef5a2d68da86329b
alisaidgunes <alisaidgunes1@gmail.com>
2023-05-26 03:55:55 +0300

    Add: service layer for department service app

---
+++ department-service/src/main/java/com/asg/departmentservice/service/Service.java
@@ -0,0 +1,7 @@
+package com.asg.departmentservice.service;
+
+import com.asg.departmentservice.dto.DepartmentDto;
+
+public interface Service {
+    DepartmentDto save(DepartmentDto departmentDto);
+}
