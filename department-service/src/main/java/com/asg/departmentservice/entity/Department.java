0b6a0f58479a0afcba4821f3068d001ee72cb81c
alisaidgunes <alisaidgunes1@gmail.com>
2023-05-26 03:32:24 +0300

    Add: Department entity for department service

---
+++ department-service/src/main/java/com/asg/departmentservice/entity/Department.java
@@ -0,0 +1,24 @@
+package com.asg.departmentservice.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="departments")
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @Column(name = "name",nullable = false)
+    private String name;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "code",nullable = false)
+    private String code;
+
+}
